0d82f742a8e09fd1ddb7b49d2f7809aa02191832
Chrizza Mae Ecal <ecalchrizzamae@gmail.com>
2022-02-24 21:54:44 +0800

    connect mongodb to django

--- BUbotProject/settings.py
+++ BUbotProject/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'BUbotProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'bubotdatabase',
+        # 'CLIENT': {
+        #    'host': 'your-db-host',
+        # }
     }
 }
 
