56e696de25b0b2818dd85c3233bc3c17814ff4cd
Chrizza Mae Ecal <ecalchrizzamae@gmail.com>
2022-02-25 20:34:41 +0800

    edited files for pwa

--- BUbotApp/settings.py
+++ BUbotApp/settings.py
@@ -135,6 +135,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
+#to disable the the console.log on browser
+PWA_APP_DEBUG_MODE = False
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- BUbotProject/urls.py
+++ BUbotProject/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, url
 # from django.urls import re_path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('BUbotApp.urls')),
-    path('', include('pwa.urls')),
+    path('admin/', admin.site.urls),
+    # path('', include('pwa.urls')),
+    url('', include('pwa.urls'))
 ]
