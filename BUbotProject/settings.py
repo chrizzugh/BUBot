109fdd1380be8d74425191a67533349d58abceaf
Chrizza Mae Ecal <ecalchrizzamae@gmail.com>
2022-03-05 16:22:07 +0800

    fix: refactor hard coded connection string to Heroku config var

--- BUbotProject/settings.py
+++ BUbotProject/settings.py
@@ -84,6 +84,9 @@ WSGI_APPLICATION = 'BUbotProject.wsgi.application'
 #     }
 # }
 
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+
 
 DATABASES = {
     'default': {
