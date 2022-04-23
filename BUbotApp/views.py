f9af1912c3fdb1a47b9c4851e964ab1731cd2c3f
unknown <redenmarlargo.bellen@bicol-u.edu.ph>
2022-04-23 21:23:03 +0800

    views.py updated

--- BUbotApp/views.py
+++ BUbotApp/views.py
@@ -88,6 +88,36 @@ def prep_ques(txt, abbrev):
 
     return ' '.join(txt)
 
+
+def prep_ans(txt):
+
+    txt = txt.replace('how can you ', '')
+    txt = txt.replace('what are the ', '')
+    txt = txt.replace('who is the ', '')
+    txt = txt.replace('how can I ', '')
+    txt = txt.replace('can I ', '')
+    txt = txt.replace('why do ', '')
+    txt = txt.replace('what is ', '')
+    txt = txt.replace('what are ', '')
+    txt = txt.replace('what is ', '')
+    txt = txt.replace('who can ', '')
+    txt = txt.replace('is there ', '')
+    txt = txt.replace('do i need ', '')
+    txt = txt.replace('how to be ', '')
+    txt = txt.replace('what will ', '')
+    txt = txt.replace('how many ', '')
+    txt = txt.replace('how much is ', '')
+    txt = txt.replace('will ','')
+    txt = txt.replace('do I ', '')
+    txt = txt.replace('what time does', "Time")
+    txt = txt.replace('what medical services does the', 'Medical services')
+    txt = txt.replace('what dental services does the', 'Dental services')
+    
+    txt = txt.replace(', how can I help', '')
+    txt = txt.replace('?', '')
+    
+    return txt.capitalize()
+
 #--------code for the website starts here------------
 import nltk 
 # nltk.download('stopwords') #for first run
@@ -112,9 +142,11 @@ def chat(request):
     if request.method == "POST":
         userQuery = request.POST ['userInput']
         userQuery = prep_ques(userQuery, abbrev)
+        if userQuery == 'bucet':
+            userQuery = userQuery.replace('bucet','bucess')
         predicted = Bubot(userQuery).numpy()
         bubotResponse = predicted.decode()
-        
+        bubotResponse = prep_ans(bubotResponse)
         return JsonResponse(bubotResponse, safe=False)
     
     return render(request, "chat.html")
